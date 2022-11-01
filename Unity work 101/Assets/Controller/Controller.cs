0eb2f25df59b4c878c66aa2f03e5ebc0ee3968d4
znotkyz <not9106@gmail.com>
2022-11-01 11:58:18 +0700

    Update Controller

---
+++ Unity work 101/Assets/Controller/Controller.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print(transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print("Hello");
+        print(transform.position);
+
+
+        if(Input.GetKey(KeyCode.W) == true)
+        {
+            //print("W");
+
+            //transform.position = new Vector3(0, 2, 0);
+
+            transform.position = new Vector3(0, transform.position.y + 0.001f, 0);
+
+        }
+
+        if (Input.GetKey(KeyCode.S) == true)
+        {
+
+            //transform.position = new Vector3(0, -2, 0);
+
+            transform.position = new Vector3(0, transform.position.y + -0.001f, 0);
+        }
+
+
+        if (Input.GetKey(KeyCode.A) == true)
+        {
+
+            //transform.position = new Vector3(-2, 0, 0);
+
+            transform.position = new Vector3(transform.position.x + -0.001f, 0, 0);
+        }
+
+
+        if (Input.GetKey(KeyCode.D) == true)
+        {
+
+            //transform.position = new Vector3(2, 0, 0);
+
+            transform.position = new Vector3(transform.position.x + 0.001f, 0, 0);
+        }
+
+
+
+    }
+}
